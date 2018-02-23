3fa622da12a48c525ddff6157d1befcfc914fc24
momoko8443 <momoko8443@163.com>
2018-02-23 23:12:57 +0800

    update to httpd

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var https =  require('https');
+var fs = require('fs');
 var app = express();
-
-
+var key = fs.readFileSync('./cert/survey-me.pem');
+var cert = fs.readFileSync('./cert/survey-me.cert.pem');
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -20,7 +22,7 @@ app.post('/', function (req, res, next) {
     res.send({'username':'momoko'});
 });
 
-var server = app.listen(3000, function () {
+var server = https.createServer({key:key,cert:cert},app).listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
